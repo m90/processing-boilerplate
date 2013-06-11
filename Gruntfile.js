8a2ea768a079ee2336a9fae7d3348127a5b7da2a
m90 <frederik.ring@gmail.com>
2013-06-11 14:29:35 +0200

    initital commit - just a demo

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+var connect = require('connect');
+
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		watch: {
+			files: ['sketch/*.pde'],
+			tasks: ['concat', 'connect']
+		},
+
+			concat : {
+			build : {
+				src: ['sketch/*.pde',],
+				dest: 'build/build.pde'
+			}
+		},
+		exec: {
+			start_server: {
+				command: 'grunt connect:server &'
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-exec');
+
+	grunt.registerTask('default', ['concat' /*initial build*/, 'connect' /* connect's callback will call the watch task */]);
+
+	var connect = require('connect');
+
+	grunt.registerTask('connect', 'Start a custom static web server.', function() {
+
+		grunt.log.writeln('Starting static web server serving build/index.html on localhost port 9001.');
+		connect(connect.static('build')).listen(9001);
+
+		grunt.task.run('watch');
+
+	});
+
+
+};
